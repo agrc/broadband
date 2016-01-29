411f146687761919e51bc333dd794c7e3272d651
Scott Davis <stdavis@utah.gov>
2016-01-29 16:42:28 -0700

    update for latest schema

--- scripts/ubbweb_validate.py
+++ scripts/ubbweb_validate.py
@@ -10,75 +10,59 @@ from agrc import logging
 logger = logging.Logger()
 
 # variables
-pathToSDE = r"C:\PythonScripts\DatabaseConnections\UBBMAP.sde"
-fltr = "Provider_Type = 1 AND NOT (UTProvCode IN ('Strata'))"
-fltr_wireless = "TRANSTECH <> 60"
-fcs = [
-       ['UBBMAP.UBBADMIN.BB_Service_Buffered_Road', fltr],
-       ['UBBMAP.UBBADMIN.BB_Service_CensusBlock', fltr],
-       ['UBBMAP.UBBADMIN.BB_Service_Wireless', fltr_wireless],
-       ['UBBMAP.UBBADMIN.BB_SpecialCoverage_Polygons', "1 = 1"]
-       ]
+pathToSDE = r"UBBMAP.sde"
+fltr = "TRANSTECH <> 60"
+fc = 'UBBMAP.UBBADMIN.BB_Service'
 coverageFieldName = "UTProvCode"
 providerTableName = "UBBMAP.UBBADMIN.BB_Providers_Table"
 providerTableFieldName = "Code"
-coverageProviders = [] # list to hold all providers in coverage data
-tableProviders = [] # list of all providers in providers table
-nonNullFields = [
-                 '"' + coverageFieldName + '" IS NULL OR "' + coverageFieldName + '" = \'\'',
-                 '"MAXADUP" IS NULL OR "MAXADUP" = \'\'',
-                 '"MAXADDOWN" IS NULL OR "MAXADDOWN" = \'\'',
-                 '"TRANSTECH" IS NULL OR "TRANSTECH" = 0'
-                 ]
+coverageProviders = []  # list to hold all providers in coverage data
+tableProviders = []  # list of all providers in providers table
+nonNullFields = ['"' + coverageFieldName + '" IS NULL OR "' + coverageFieldName + '" = \'\'',
+                 '"MAXADUP" IS NULL OR "MAXADUP" = 0',
+                 '"MAXADDOWN" IS NULL OR "MAXADDOWN" = 0',
+                 '"TRANSTECH" IS NULL OR "TRANSTECH" = 0']
 errors = []
-mapServices = [
-               'Broadband/ProviderCoverage',
-               'Broadband/ProviderCoverageCached',
-               'Broadband/Basemaps'
-               ]
 
 try:
     logger.logMsg('setting workspace to sde database')
     arcpy.env.workspace = pathToSDE
+    arcpy.env.geographicTransformations = 'WGS_1984_(ITRF00)_To_NAD_1983'
 
-    # loop through coverage feature classes
-    logger.logMsg('Looping through polygon feature classes')
-    for fc in fcs:
-        logger.logMsg(fc[0])
+    logger.logMsg("checking non-null fields")
 
-        logger.logMsg("checking non-null fields")
+    # create layer for selecting
+    logger.logMsg('creating layer')
+    layerName = fc + 'Layer'
+    arcpy.MakeFeatureLayer_management(fc, layerName, fltr);
+    logger.logGPMsg()
 
-        # create layer for selecting
-        logger.logMsg('creating layer')
-        layerName = fc[0] + 'Layer'
-        arcpy.MakeFeatureLayer_management(fc[0], layerName, fc[1]);
+    # loop through fields
+    for query in nonNullFields:
+        logger.logMsg('query: ' + query)
+        arcpy.SelectLayerByAttribute_management(layerName, 'NEW_SELECTION', query)
+        logger.logGPMsg()
+        cnt = arcpy.GetCount_management(layerName)
         logger.logGPMsg()
 
-        # loop through fields
-        for query in nonNullFields:
-            logger.logMsg('query: ' + query)
-            arcpy.SelectLayerByAttribute_management(layerName, 'NEW_SELECTION', query)
-            logger.logGPMsg()
-            cnt = arcpy.GetCount_management(layerName)
-            logger.logGPMsg()
+        if int(str(cnt)) > 0:
+            errors.append('ERROR: null or empty values found in ' + fc + ':' + query)
 
-            if int(str(cnt)) > 0:
-                errors.append('ERROR: null or empty values found in ' + fc[0] + ':' + query)
+    # get search cursor
+    logger.logMsg("building list of providers in coverage feature class")
+    cur = arcpy.SearchCursor(fc, fltr, "", coverageFieldName)
+    row = cur.next()
+    while row:
+        code = row.getValue(coverageFieldName)
 
-        # get search cursor
-        logger.logMsg("building list of providers in coverage feature class")
-        cur = arcpy.SearchCursor(fc[0], fc[1], "", coverageFieldName)
+        # add to list of providers
+        if not [code, fc] in coverageProviders:
+            coverageProviders.append([code, fc])
+
         row = cur.next()
-        while row:
-            code = row.getValue(coverageFieldName)
+    del cur
 
-            # add to list of providers
-            if not [code, fc[0]] in coverageProviders:
-                coverageProviders.append([code, fc[0]])
-
-            row = cur.next()
-
-        logger.logMsg("Finished with " + fc[0])
+    logger.logMsg("Finished with " + fc)
 
     # get cursor for provider table
     logger.logMsg("building list of providers in providers table")
@@ -88,6 +72,7 @@ try:
         tableProviders.append(row.getValue(providerTableFieldName))
 
         row = prows.next()
+    del prows, row
 
     # loop through coverage providers and make sure that they are in the provider table list
     logger.logMsg('looking for providers that show up in coverage data but not providers table')
