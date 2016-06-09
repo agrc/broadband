2c3478f9caa6bb6a49507a32e686659a9b600433
Scott Davis <stdavis@utah.gov>
2016-06-09 16:59:48 -0600

    Implement :trackor:

--- scripts/Update_bbweb.py
+++ scripts/Update_bbweb.py
@@ -1,180 +1,135 @@
 import arcpy
-from agrc import logging, messaging, ags
 from settings import *
+from forklift.models import Pallet
+from forklift.core import check_schema
+from os.path import dirname, join
 
 
-# create new tools object
-logger = logging.Logger()
-emailer = messaging.Emailer('stdavis@utah.gov', not sendEmails)
+class BroadbandPallet(Pallet):
+    def __init__(self):
+        super(BroadbandPallet, self).__init__()
 
-# variables
-pathToSDE = r".\UBBMAP.sde"
-pathToFGD = r".\Broadband.gdb"
+        destination_workspace = r'C:\ForkliftData\Broadband.gdb'
+        source_workspace = join(dirname(__file__), 'UBBMAP.sde')
 
-fltr = "TRANSTECH <> 60"
-fc = 'UBBMAP.UBBADMIN.BB_Service'
-coverageFieldName = "UTProvCode"
-providerTableName = "UBBMAP.UBBADMIN.BB_Providers_Table"
-providerTableFieldName = "Code"
-coverageProviders = []  # list to hold all providers in coverage data
-tableProviders = []  # list of all providers in providers table
-nonNullFields = ['"' + coverageFieldName + '" IS NULL OR "' + coverageFieldName + '" = \'\'',
-                 '"MAXADUP" IS NULL OR "MAXADUP" = 0',
-                 '"MAXADDOWN" IS NULL OR "MAXADDOWN" = 0',
-                 '"TRANSTECH" IS NULL OR "TRANSTECH" = 0']
-errors = []
-mapServices = ['Broadband/ProviderCoverage',
-               'Broadband/WirelineCached',
-               'Broadband/FixedCached',
-               'Broadband/MobileCached',
-               'BBEcon/MapService']
-cachedServices = ['Wireline', 'Fixed', 'Mobile']
-scales = [
-    591657527.591555,
-    295828763.795777,
-    147914381.897889,
-    73957190.948944,
-    36978595.474472,
-    18489297.737236,
-    9244648.868618,
-    4622324.434309,
-    2311162.217155,
-    1155581.108577,
-    577790.554289,
-    288895.277144,
-    144447.638572,
-    72223.819286
-]
+        self.add_crates(['BB_Service', 'BB_Providers_Table'],
+                        {'source_workspace': source_workspace, 'destination_workspace': destination_workspace})
 
-try:
-    logger.logMsg('setting workspace to sde database')
-    arcpy.env.workspace = pathToSDE
-    arcpy.env.geographicTransformations = 'NAD_1983_To_WGS_1984_5'
+        self.copy_data = [destination_workspace]
 
-    logger.logMsg("checking non-null fields")
+    def validate_crate(self, crate):
+        fltr = 'TRANSTECH <> 60'
+        bb_service = 'UBBMAP.UBBADMIN.BB_Service'
+        providerTableName = 'UBBMAP.UBBADMIN.BB_Providers_Table'
+        providerTableFieldName = 'Code'
+        coverageProviders = []  # list to hold all providers in coverage data
+        tableProviders = []  # list of all providers in providers table
+        coverageFieldName = 'UTProvCode'
+        nonNullFields = ['"' + coverageFieldName + '" IS NULL OR "' + coverageFieldName + '" = \'\'',
+                         '"MAXADUP" IS NULL OR "MAXADUP" = 0',
+                         '"MAXADDOWN" IS NULL OR "MAXADDOWN" = 0',
+                         '"TRANSTECH" IS NULL OR "TRANSTECH" = 0']
+        errors = []
 
-    # create layer for selecting
-    logger.logMsg('creating layer')
-    layerName = fc + 'Layer'
-    arcpy.MakeFeatureLayer_management(fc, layerName, fltr);
-    logger.logGPMsg()
+        if crate.source_name != 'BB_Service':
+            return NotImplemented
 
-    # loop through fields
-    for query in nonNullFields:
-        logger.logMsg('query: ' + query)
-        arcpy.SelectLayerByAttribute_management(layerName, 'NEW_SELECTION', query)
-        logger.logGPMsg()
-        cnt = arcpy.GetCount_management(layerName)
-        logger.logGPMsg()
+        if not check_schema(crate.source, crate.destination):
+            return False
 
-        if int(str(cnt)) > 0:
-            errors.append('ERROR: null or empty values found in ' + fc + ':' + query)
+        arcpy.env.workspace = crate.source_workspace
+        arcpy.env.geographicTransformations = 'NAD_1983_To_WGS_1984_5'
 
-    # get search cursor
-    logger.logMsg("building list of providers in coverage feature class")
-    cur = arcpy.SearchCursor(fc, fltr, "", coverageFieldName)
-    row = cur.next()
-    while row:
-        code = row.getValue(coverageFieldName)
+        self.log.info("checking non-null fields")
 
-        # add to list of providers
-        if not [code, fc] in coverageProviders:
-            coverageProviders.append([code, fc])
+        # create layer for selecting
+        self.log.info('creating layer')
+        layerName = bb_service + 'Layer'
+        arcpy.MakeFeatureLayer_management(bb_service, layerName, fltr)
 
+        # loop through fields
+        for query in nonNullFields:
+            self.log.info('query: ' + query)
+            arcpy.SelectLayerByAttribute_management(layerName, 'NEW_SELECTION', query)
+            cnt = arcpy.GetCount_management(layerName)
+
+            if int(str(cnt)) > 0:
+                errors.append('ERROR: null or empty values found in ' + bb_service + ':' + query)
+
+        # get search cursor
+        self.log.info('building list of providers in coverage feature class')
+        cur = arcpy.SearchCursor(bb_service, fltr, '', coverageFieldName)
         row = cur.next()
-    del cur
+        while row:
+            code = row.getValue(coverageFieldName)
 
-    logger.logMsg("Finished with " + fc)
+            # add to list of providers
+            if not [code, bb_service] in coverageProviders:
+                coverageProviders.append([code, bb_service])
 
-    # get cursor for provider table
-    logger.logMsg("building list of providers in providers table")
-    prows = arcpy.SearchCursor(providerTableName, "Exclude IS NULL OR Exclude = ''")
-    row = prows.next()
-    while row:
-        tableProviders.append(row.getValue(providerTableFieldName))
+            row = cur.next()
+        del cur
 
+        self.log.info('Finished with ' + bb_service)
+
+        # get cursor for provider table
+        self.log.info('building list of providers in providers table')
+        prows = arcpy.SearchCursor(providerTableName, "Exclude IS NULL OR Exclude = ''")
         row = prows.next()
-    del prows, row
+        while row:
+            tableProviders.append(row.getValue(providerTableFieldName))
 
-    # loop through coverage providers and make sure that they are in the provider table list
-    logger.logMsg('looking for providers that show up in coverage data but not providers table')
-    missingProviders = []
-    for row in coverageProviders:
-        if not row[0] in tableProviders:
-            missingProviders.append(row)
+            row = prows.next()
+        del prows, row
 
-    # check for data errors
-    if len(errors) > 0:
-        logger.logMsg("ERRORS IN DATA:")
-        for e in errors:
-            logger.logMsg(e)
-    else:
-        logger.logMsg("NO ERRORS IN DATA")
+        # loop through coverage providers and make sure that they are in the provider table list
+        self.log.info('looking for providers that show up in coverage data but not providers table')
+        missingProviders = []
+        for row in coverageProviders:
+            if not row[0] in tableProviders:
+                missingProviders.append(row)
 
-    # check for mis matching providers
-    if len(missingProviders) > 0:
-        logger.logMsg("MISSING PROVIDERS THAT ARE IN THE COVERAGE DATA BUT NOT IN THE PROVIDERS TABLE:")
-        for mp in missingProviders:
-            logger.logMsg(str(mp))
-    else:
-        logger.logMsg("NO PROVIDERS FOUND IN THE COVERAGE DATA THAT ARE NOT IN THE PROVIDERS TABLE.")
+        # check for data errors
+        if len(errors) > 0:
+            self.log.info('ERRORS IN DATA:')
+            for e in errors:
+                self.log.info(e)
+        else:
+            self.log.info('NO ERRORS IN DATA')
 
-    # if errors, bail on script and send an email
-    if len(missingProviders) > 0 or len(errors) > 0:
-        logger.logMsg('Sending error email')
-        emailer.sendEmail('Update_bbweb has found errors', logger.log)
-    else:
-        # delete all feature classes
-        logger.logMsg('switching workspace to local filegeodatabase')
-        arcpy.env.workspace = pathToFGD
+        # check for mis matching providers
+        if len(missingProviders) > 0:
+            self.log.info('MISSING PROVIDERS THAT ARE IN THE COVERAGE DATA BUT NOT IN THE PROVIDERS TABLE:')
+            for mp in missingProviders:
+                self.log.info(str(mp))
+        else:
+            self.log.info('NO PROVIDERS FOUND IN THE COVERAGE DATA THAT ARE NOT IN THE PROVIDERS TABLE.')
 
-        logger.logMsg('deleting all feature classes')
-        fClasses = arcpy.ListFeatureClasses()
-        for c in fClasses:
-            arcpy.Delete_management(c)
-            logger.logGPMsg()
+        if len(missingProviders) > 0 or len(errors) > 0:
+            self.log.info('Errors were found during validation: %s', errors)
+            return False
 
-        logger.logMsg('importing new feature classes')
-        arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(3857)
-        fgdbFCName = fc.split('.')[2]
-        arcpy.FeatureClassToFeatureClass_conversion(pathToSDE + '\\' + fc, pathToFGD, fgdbFCName + '_temp', fltr)
-        arcpy.Dissolve_management(fgdbFCName + '_temp', fgdbFCName, ['UTProvCode', 'TransTech', 'MAXADDOWN', 'MAXADUP', 'LastVerified'], multi_part='SINGLE_PART')
-        arcpy.Delete_management(fgdbFCName + '_temp')
+        return True
 
-        # provider table
-        arcpy.Delete_management(pathToFGD + '\\' + providerTableName.split('.')[2])
-        arcpy.TableToTable_conversion(pathToSDE + '\\' + providerTableName, pathToFGD, providerTableName.split('.')[2])
-        logger.logGPMsg()
+    def process(self):
+        cachedServices = ['Wireline', 'Fixed', 'Mobile']
+        scales = [
+            591657527.591555,
+            295828763.795777,
+            147914381.897889,
+            73957190.948944,
+            36978595.474472,
+            18489297.737236,
+            9244648.868618,
+            4622324.434309,
+            2311162.217155,
+            1155581.108577,
+            577790.554289,
+            288895.277144,
+            144447.638572,
+            72223.819286
+        ]
 
-        admin = ags.AGSAdmin(AGS_USER, AGS_PASSWORD, AGS_SERVER)
-
-        for s in mapServices:
-            logger.logMsg('stopping {}'.format(s))
-            admin.stopService(s, 'MapServer')
-
-        arcpy.Delete_management(pathToProdFGD)
-        logger.logGPMsg()
-        arcpy.Copy_management(pathToFGD, pathToProdFGD)
-        logger.logGPMsg()
-
-        for s in mapServices:
-            logger.logMsg('starting {}'.format(s))
-            admin.startService(s, 'MapServer')
-
-        logger.logMsg('recaching')
-
+        self.log.info('recaching')
         for cs in cachedServices:
             arcpy.ManageMapServerCacheTiles_server(cachedServiceBase.format(cs), scales, 'RECREATE_ALL_TILES', 1)
-            logger.logGPMsg()
-
-        emailer.sendEmail('Update_bbweb.py has run successfully', "Nice work!")
-
-except arcpy.ExecuteError:
-    logger.logMsg('arcpy.ExecuteError')
-    logger.logError()
-    logger.logGPMsg()
-    emailer.sendEmail(logger.scriptName + ' - arcpy.ExecuteError', logger.log)
-
-except:
-    logger.logError()
-    emailer.sendEmail(logger.scriptName + ' - Python Error', logger.log)
